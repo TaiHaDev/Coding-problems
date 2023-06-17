d5d272655ca6e47c0c5f259742de524ca0d56f2a
Tai Ha <u7454578@anu.edu.au>
2023-06-17 19:03:00 +1000

    overtime
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/not_leetcode/PickCard.java
@@ -0,0 +1,29 @@
+package not_leetcode;
+
+import java.util.Scanner;
+
+public class PickCard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int[] cards = new int[total];
+        for (int i = 0; i < total; i++) {
+            cards[i] = sc.nextInt();
+        }
+        int[] result = {0, 0};
+        int turn = 0;
+        // two pointers
+        int left = 0, right = total - 1;
+        while (left <= right) {
+            if (cards[left] > cards[right]) {
+                result[turn] += cards[left];
+                left++;
+            } else {
+                result[turn] += cards[right];
+                right--;
+            }
+            turn ^= 1;
+        }
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
