9664f06d28852874e7dbb0158d4cee533e51bff3
Tai Ha <u7454578@anu.edu.au>
2023-06-24 18:20:38 +1000

    using adjacency list instead adjacency matrix
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/not_leetcode/graph/dfs/LongestStreet.java
+++ src/main/java/not_leetcode/graph/dfs/LongestStreet.java
@@ -1,5 +1,7 @@
 package not_leetcode.graph.dfs;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LongestStreet {
@@ -8,34 +10,47 @@ public class LongestStreet {
         int n = sc.nextInt();
         while (n-- > 0) {
             int vertices = sc.nextInt();
-            int[][] adjMatrix = new int[vertices][vertices];
+            List<TownStreet>[] adjList = new List[vertices];
+            for (int i = 0; i < vertices; i++) {
+                adjList[i] = new ArrayList<>();
+            }
             for (int i = 1; i < vertices; i++) {
-                int v1 = sc.nextInt();
-                int v2 = sc.nextInt();
+                int v1 = sc.nextInt() - 1;
+                int v2 = sc.nextInt() - 1;
                 int length = sc.nextInt();
-                adjMatrix[v1 - 1][v2 - 1] = length;
-                adjMatrix[v2 - 1][v1 - 1] = length;
+                adjList[v1].add(new TownStreet(v2, length));
+                adjList[v2].add(new TownStreet(v1, length));
             }
                int result = 0;
             for (int i = 1; i < vertices; i++) {
-                result = Math.max(result, dfs(adjMatrix, i, new boolean[vertices]));
+                result = Math.max(result, dfs(adjList, i, new boolean[vertices]));
             }
             System.out.println(result);
         }
     }
 
-    private static int dfs(int[][] adjMatrix, int vertex, boolean[] visited) {
+    private static int dfs(List<TownStreet>[] adjMatrix, int vertex, boolean[] visited) {
         if (visited[vertex - 1]) return 0;
-        int[] matrix = adjMatrix[vertex - 1];
+        List<TownStreet> matrix = adjMatrix[vertex - 1];
         int result = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            int weight = matrix[i];
+        for (TownStreet street : matrix) {
+            int weight = street.weight;
+            int i = street.to;
             if (weight > 0 && !visited[i]) {
                 visited[vertex - 1] = true;
                 result = Math.max(result, weight + dfs(adjMatrix, i + 1, visited));
-                visited[vertex - 1] = false;
             }
         }
         return result;
+    }
+}
+
+class TownStreet {
+    int to;
+    int weight;
+
+    public TownStreet(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
 }
