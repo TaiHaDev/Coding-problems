5c2681a43bec7e5920ca8ecbcec00c6414cf94cf
Tai Ha <u7454578@anu.edu.au>
2023-07-10 13:29:41 +1000

    so hard
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/big_o/graph/floydwarshall/AsteriskAndObelisk.java
+++ src/main/java/big_o/graph/floydwarshall/AsteriskAndObelisk.java
@@ -1,62 +1,169 @@
 package big_o.graph.floydwarshall;
 
-import java.util.*;
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class AsteriskAndObelisk {
-    static final int INF = 1000000000;
-    static int[][] dist;
-    static int[] feastCost;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
-        int t = 1;
+        Reader sc = new Reader();
+        int c = 1;
         while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int C = Integer.parseInt(st.nextToken());
-            int S = Integer.parseInt(st.nextToken());
-            int Q = Integer.parseInt(st.nextToken());
-            if (C == 0 && S == 0 && Q == 0)
-                break;
-
-            dist = new int[C][C];
-            for (int i = 0; i < C; i++)
-                Arrays.fill(dist[i], INF);
-
-            feastCost = new int[C];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < C; i++) {
-                feastCost[i] = Integer.parseInt(st.nextToken());
-                dist[i][i] = feastCost[i];
+            int vertices = sc.nextInt();
+            int edges = sc.nextInt();
+            int queries = sc.nextInt();
+            if (vertices == 0 && edges == 0 && queries == 0) break;
+            int[] feasts = new int[vertices];
+            for (int i = 0; i < vertices; i++) {
+                feasts[i] = sc.nextInt();
             }
+            int[][][] adjMatrix = new int[vertices][vertices][2]; // [weight, feast]
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    adjMatrix[i][j] = i == j ? new int[] {0, feasts[i]} : new int[] {Integer.MAX_VALUE, feasts[i]};
+                }
 
-            for (int i = 0; i < S; i++) {
-                st = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(st.nextToken()) - 1;
-                int v = Integer.parseInt(st.nextToken()) - 1;
-                int w = Integer.parseInt(st.nextToken());
-                dist[u][v] = dist[v][u] = Math.max(w, Math.max(feastCost[u], feastCost[v]));
             }
-
-            for (int k = 0; k < C; k++)
-                for (int i = 0; i < C; i++)
-                    for (int j = 0; j < C; j++)
-                        dist[i][j] = Math.min(dist[i][j], Math.max(dist[i][k], dist[k][j]));
-
-            if (t != 1)
-                pw.println();
-            pw.println("Case #" + t++);
-            for (int q = 0; q < Q; q++) {
-                st = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(st.nextToken()) - 1;
-                int v = Integer.parseInt(st.nextToken()) - 1;
-                if (dist[u][v] == INF)
-                    pw.println("-1");
-                else
-                    pw.println(dist[u][v]);
+            for (int i = 0; i < edges; i++) {
+                int u = sc.nextInt() -1;
+                int v = sc.nextInt() - 1;
+                int w = sc.nextInt();
+                adjMatrix[u][v] = new int[] {w, Math.max(feasts[u], feasts[v])};
+                adjMatrix[v][u] = new int[] {w, Math.max(feasts[u], feasts[v])};
+            }
+            floydwarshall(adjMatrix, vertices);
+            System.out.println("Case #" + c++);
+            for (int i = 0; i < queries; i++) {
+                int start = sc.nextInt() - 1;
+                int des = sc.nextInt() - 1;
+                System.out.println(adjMatrix[start][des][1] + adjMatrix[start][des][0]);
             }
         }
-        pw.close();
+    }
+
+    private static void floydwarshall(int[][][] adjMatrix, int vertices) {
+        for (int k = 0; k < vertices; k++) {
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    if (adjMatrix[i][k][0] != Integer.MAX_VALUE && adjMatrix[k][j][0] != Integer.MAX_VALUE && adjMatrix[i][j][0] > adjMatrix[i][k][0] + adjMatrix[k][j][0]) {
+                        adjMatrix[i][j][0] = adjMatrix[i][k][0] + adjMatrix[k][j][0];
+                        adjMatrix[i][j][1] = Math.max(adjMatrix[i][k][1], adjMatrix[k][j][1]);
+                    }
+                }
+            }
+        }
+    }
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(
+                    new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                    BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
     }
 }
