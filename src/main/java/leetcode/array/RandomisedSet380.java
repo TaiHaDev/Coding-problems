8319091bee254960874e1765c88034137218fdfb
Tai Ha <u7454578@anu.edu.au>
2023-07-15 21:12:47 +1000

    RandomisedSet380.java
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/array/RandomisedSet380.java
@@ -0,0 +1,42 @@
+package leetcode.array;
+
+import java.util.*;
+
+public class RandomisedSet380 {
+    List<Integer> nums;
+    Map<Integer, Integer> idxMap;
+    Random random;
+
+
+
+    public boolean insert(int val) {
+        if (idxMap.containsKey(val)) {
+            return false;
+        }
+
+        idxMap.put(val, nums.size());
+        nums.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!idxMap.containsKey(val)) {
+            return false;
+        }
+
+        int idx = idxMap.get(val);
+        int lastIdx = nums.size() - 1;
+        if (idx != lastIdx) {
+            int lastVal = nums.get(lastIdx);
+            nums.set(idx, lastVal);
+            idxMap.put(lastVal, idx);
+        }
+        nums.remove(lastIdx);
+        idxMap.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return nums.get(random.nextInt(nums.size()));
+    }
+}
