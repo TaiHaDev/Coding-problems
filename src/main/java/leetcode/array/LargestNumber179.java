2d82877fd81156bd721167c5d89037813fc4c103
Tai Ha <u7454578@anu.edu.au>
2023-07-21 01:02:05 +1000

    LargestNumber179.java
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/leetcode/array/LargestNumber179.java
+++ src/main/java/leetcode/array/LargestNumber179.java
@@ -1,14 +1,20 @@
 package leetcode.array;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class LargestNumber179 {
     public String largestNumber(int[] nums) {
         String[] arr = new String[nums.length];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf(nums[i]);
+            arr[i] = Integer.toString(nums[i]);
         }
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o1 + o2).compareTo(o2 + o1);
+            }
+        });
         if (arr[0].equals("0")) return "0";
         StringBuilder stringBuilder = new StringBuilder();
         for (String s : arr) {
