8db088b01e39aee1244dcbccce110d5ff4a0772a
Tai Ha <u7454578@anu.edu.au>
2023-07-20 20:41:47 +1000

    DNAPrefix.java
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/big_o/trie/DNAPrefix.java
@@ -0,0 +1,50 @@
+package big_o.trie;
+
+import java.util.Scanner;
+
+public class DNAPrefix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 1;
+        while (n-- > 0) {
+            int total = sc.nextInt();
+            Trie trie = new Trie();
+            for (int i = 0; i < total; i++) {
+                trie.addDNA(sc.next());
+            }
+            System.out.println("Case " + c++ + ": " + trie.max);
+        }
+    }
+}
+class Trie {
+    Node root = new Node();
+    int max;
+
+    public void addDNA(String dna) {
+        Node temp = root;
+        int len = 0;
+        for (char c : dna.toCharArray()) {
+            int index;
+            if (c == 'A') index = 0;
+            else if (c == 'C') index = 1;
+            else if (c == 'G') index = 2;
+            else index = 3;
+            if (temp.child[index] == null) {
+                temp.child[index] = new Node();
+            }
+            temp = temp.child[index];
+            len++;
+            temp.totalChildCount++;
+            max = Math.max(temp.totalChildCount * len, max);
+        }
+    }
+
+
+}
+class Node {
+    Node[] child = new Node[4];
+    int totalChildCount = 0;
+}
+
+
--- src/main/java/big_o/trie/SearchEngine.java
+++ src/main/java/big_o/trie/SearchEngine.java
@@ -1,63 +1,63 @@
-package big_o.trie;
-
-import java.util.Scanner;
-
-public class SearchEngine {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int entries = sc.nextInt();
-        int queries = sc.nextInt();
-        Trie trie = new Trie();
-        for (int i = 0; i < entries; i++) {
-            trie.addWord(sc.next(), sc.nextInt());
-        }
-        for (int i = 0; i < queries; i++) {
-            System.out.println(trie.findBestMatch(sc.next()));
-        }
-    }
-}
-
-
-class Trie {
-    Node root;
-
-    public Trie() {
-        this.root = new Node();
-    }
-    public void addWord(String word, int weight) {
-        Node temp = root;
-        for (char c : word.toCharArray()) {
-            temp.maxValue = Math.max(temp.maxValue, weight);
-            int index = c - 'a';
-            if (temp.child[index] == null) {
-                temp.child[index] = new Node();
-            }
-            temp = temp.child[index];
-        }
-        temp.maxValue = Math.max(temp.maxValue, weight);
-    }
-    public int findBestMatch(String word) {
-        Node temp = root;
-        for (char c : word.toCharArray()) {
-            int index = c - 'a';
-            if (temp.child[index] == null) {
-                return -1;
-            }
-            temp = temp.child[index];
-        }
-        return temp.maxValue;
-    }
-
-
-
-
-}
-
-class Node {
-
-    Node[] child = new Node[26];
-    int maxValue = 0;
-
-
-
-}
+//package big_o.trie;
+//
+//import java.util.Scanner;
+//
+//public class SearchEngine {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int entries = sc.nextInt();
+//        int queries = sc.nextInt();
+//        Trie trie = new Trie();
+//        for (int i = 0; i < entries; i++) {
+//            trie.addWord(sc.next(), sc.nextInt());
+//        }
+//        for (int i = 0; i < queries; i++) {
+//            System.out.println(trie.findBestMatch(sc.next()));
+//        }
+//    }
+//}
+//
+//
+//class Trie {
+//    Node root;
+//
+//    public Trie() {
+//        this.root = new Node();
+//    }
+//    public void addWord(String word, int weight) {
+//        Node temp = root;
+//        for (char c : word.toCharArray()) {
+//            temp.maxValue = Math.max(temp.maxValue, weight);
+//            int index = c - 'a';
+//            if (temp.child[index] == null) {
+//                temp.child[index] = new Node();
+//            }
+//            temp = temp.child[index];
+//        }
+//        temp.maxValue = Math.max(temp.maxValue, weight);
+//    }
+//    public int findBestMatch(String word) {
+//        Node temp = root;
+//        for (char c : word.toCharArray()) {
+//            int index = c - 'a';
+//            if (temp.child[index] == null) {
+//                return -1;
+//            }
+//            temp = temp.child[index];
+//        }
+//        return temp.maxValue;
+//    }
+//
+//
+//
+//
+//}
+//
+//class Node {
+//
+//    Node[] child = new Node[26];
+//    int maxValue = 0;
+//
+//
+//
+//}
