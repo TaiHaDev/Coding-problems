8ad86272972d48567f31a47c0a5a120f1f1942d2
Tai Ha <u7454578@anu.edu.au>
2023-07-20 17:41:11 +1000

    First trie problem ever, I did pretty good
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/array/GridGame.java
@@ -0,0 +1,15 @@
+package leetcode.array;
+
+public class GridGame {
+
+    public long gridGame(int[][] grid) {
+        return 0;
+    }
+    public int backtrack(int[][] grid) {
+        int r = 0;
+        for (int i = 0; i < grid[0].length - 1; i++) {
+
+        }
+        return 0;
+    }
+}
