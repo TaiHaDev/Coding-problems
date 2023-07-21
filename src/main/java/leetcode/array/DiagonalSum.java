00a919ae1f3166935246f2776772070e5669b466
Tai Ha <u7454578@anu.edu.au>
2023-07-22 01:39:10 +1000

    math
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/array/DiagonalSum.java
@@ -0,0 +1,14 @@
+package leetcode.array;
+
+public class DiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int result = 0;
+        for (int i = 0; i < mat.length; i++) {
+            result += mat[i][i];
+            if (mat.length % 2 != 0 && i == mat.length / 2) continue;
+            result += mat[i][mat[0].length - 1 - i];
+        }
+        return result;
+    }
+}
+}
