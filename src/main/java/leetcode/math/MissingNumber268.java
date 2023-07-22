db85979731fe5eacdc89dbac0f4b44c60cf0c9af
Tai Ha <u7454578@anu.edu.au>
2023-07-23 00:53:24 +1000

    MissingNumber268.java
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/leetcode/math/MissingNumber268.java
+++ src/main/java/leetcode/math/MissingNumber268.java
@@ -1,13 +1,19 @@
 package leetcode.math;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class MissingNumber268 {
-    public int missingNumber(int[] nums) {
-        int max = nums.length;
-        int total = 0;
-        for (int num : nums) {
-            total += num;
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        LinkedList<Integer> result = new LinkedList();
+        for (int i = num.length - 1; i >= 0; i--) {
+            result.add(0, (num[i] + k) % 10);
+            k = (num[i] + k) / 10;
         }
-        int realTotal = max * (max + 1) / 2;
-        return realTotal - total;
+        while(k > 0) {
+            result.add(0, k%10);
+            k /= 10;
+        }
+        return result;
     }
 }
