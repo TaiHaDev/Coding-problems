d71bb9b6176df47a846b87f4bb3dfc7efea87f72
Tai Ha <u7454578@anu.edu.au>
2023-07-22 22:12:38 +1000

    trie
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/big_o/trie/NoPrefixSet.java
+++ src/main/java/big_o/trie/NoPrefixSet.java
@@ -1,29 +1,58 @@
 package big_o.trie;
 
+import java.util.Scanner;
+
 public class NoPrefixSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        Trie trie = new Trie();
+        String result = null;
+        for (int i = 0; i < total; i++) {
+            String cur = sc.next();
+            if (!trie.addNumber(cur)) {
+                result = cur;
+                break;
+            }
+        }
+        if (result != null) {
+            System.out.println("BAD SET");
+            System.out.println(result);
+        } else {
+            System.out.println("GOOD SET");
+        }
+    }
     static class Trie {
         Node root = new Node();
 
-        public void addNumber(String number) {
+        public boolean addNumber(String number) {
             Node temp = root;
-            char[] charArray = number.toCharArray();
+
             boolean isPrefix = false;
-            for (int i = 0; i < charArray.length; i++) {
-                char c = charArray[i];
+            for (int i = 0; i < number.length(); i++) {
+                char c = number.charAt(i);
                 int index = Character.getNumericValue(c);
                 if (temp.child[index] == null) {
                     temp.child[index] = new Node();
                 } else {
+                    if (temp.child[index].wordCount != 0 || (i == number.length() - 1 && !isEmpty(temp.child[index]))) return false;
                 }
                 temp = temp.child[index];
             }
             temp.wordCount = 1;
+            return true;
+        }
+        private boolean isEmpty(Node node) {
+            for (int i = 0; i < 26; i++) {
+                if (node.child[i] != null) return false;
+            }
+            return true;
         }
 
     }
 
     static class Node {
-       Node[] child = new Node[10];
+       Node[] child = new Node[26];
         int wordCount = 0;
     }
 }
