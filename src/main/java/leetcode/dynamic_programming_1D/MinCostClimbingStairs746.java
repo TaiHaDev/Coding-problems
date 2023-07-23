8cc377a6be8a09170ead528d83d135821bf7a792
Tai Ha <u7454578@anu.edu.au>
2023-07-24 01:10:55 +1000

    MinCostClimbingStairs746.java
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/leetcode/dynamic_programming_1D/MinCostClimbingStairs746.java
+++ src/main/java/leetcode/dynamic_programming_1D/MinCostClimbingStairs746.java
@@ -7,12 +7,9 @@ public class MinCostClimbingStairs746 {
     }
 
     public int minCostClimbingStairs(int[] cost) {
-//        Map<Integer, Integer> costMap = new HashMap<>();
-//        costMap.put(cost.length, 0);
-//        costMap.put(cost.length + 1, 0);
-        for (int i = cost.length - 3; i >= 0; i--) {
-            cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] +=  Math.min(cost[i-2] , cost[i-1]);
         }
-        return Math.min(cost[0], cost[1]);
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
     }
 }
