9c32f29a87694c0c7108b9091571c54d5d4b729a
Tai Ha <u7454578@anu.edu.au>
2023-08-03 00:29:09 +1000

    SumOfLeftLeaves
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/tree/SumOfLeftLeaves.java
@@ -0,0 +1,11 @@
+package leetcode.tree;
+
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.left.right == null && root.left.left == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
