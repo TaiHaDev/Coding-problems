4ccb3addfe0c979e2d09d5f21d782950d2485e48
Tai Ha <u7454578@anu.edu.au>
2023-08-08 01:27:54 +1000

    FindTheDifference389
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/no_category/FindTheDifference389.java
@@ -0,0 +1,10 @@
+package leetcode.no_category;
+
+public class FindTheDifference389 {
+    public char findTheDifference(String s, String t) {
+        int ans = 0;
+        for (char c : t.toCharArray()) ans += c;
+        for (char c : s.toCharArray()) ans -= c;
+        return (char) ans;
+    }
+}
