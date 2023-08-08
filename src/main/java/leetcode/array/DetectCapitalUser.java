81b30cb39847ba27f7fe60ee136d923d76d33d88
Tai Ha <u7454578@anu.edu.au>
2023-08-08 23:14:24 +1000

    DetectCapitalUser
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/leetcode/array/DetectCapitalUser.java
@@ -0,0 +1,11 @@
+package leetcode.array;
+
+public class DetectCapitalUser {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        for (char c : word.toCharArray()) {
+            if (Character.isUpperCase(c)) count++;
+        }
+        return count == 0 || count == word.length() || (count == 1 && Character.isUpperCase(word.charAt(0)));
+    }
+}
