7e9d2843ad7f809479dff484bfcbc0e5034f2760
Tai Ha <u7454578@anu.edu.au>
2023-08-15 00:42:23 +1000

    RemoveElement27
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/big_o/array/RemoveElement27.java
@@ -0,0 +1,13 @@
+package big_o.array;
+
+public class RemoveElement27 {
+//    public int RemoveElement(int[] nums, int val) {
+//        int index = 0;
+//        foreach (int num in nums) {
+//            if (num != val) {
+//                nums[index++] = num;
+//            }
+//        }
+//        return index;
+//    }
+}
