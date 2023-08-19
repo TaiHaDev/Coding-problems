10514dacdac8c82dcbded6c74a49c80562f87de9
Tai Ha <u7454578@anu.edu.au>
2023-08-19 17:38:51 +1000

    Topological sort using Kahn algorithm
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/big_o/topological_sort/TopologicalSorting.java
+++ src/main/java/big_o/topological_sort/TopologicalSorting.java
@@ -16,42 +16,56 @@ public class TopologicalSorting {
         Scanner sc = new Scanner(System.in);
         int vertices = sc.nextInt();
         int edges = sc.nextInt();
+
         List<List<Integer>> adjList = new ArrayList<>();
-        for (int i = 0; i < vertices + 1; i++) {
+        for (int i = 0; i <= vertices; i++) {
             adjList.add(new ArrayList<>());
         }
+
         for (int i = 0; i < edges; i++) {
-            adjList.get(sc.nextInt()).add(sc.nextInt());
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adjList.get(u).add(v);
         }
+
         List<Integer> result = new ArrayList<>();
+
         if (topologicalSort(adjList, result)) {
             for (int res : result) {
-                System.out.println(res + " ");
+                System.out.print(res + " ");
             }
         } else {
-            System.out.print("Sandro fails.");
+            System.out.println("Sandro fails.");
         }
     }
 
     private static boolean topologicalSort(List<List<Integer>> adjList, List<Integer> result) {
         int[] inDegree = new int[adjList.size()];
-        Queue<Integer> degree0 = new LinkedList<>();
+        PriorityQueue<Integer> degree0 = new PriorityQueue<>();  // Use a PriorityQueue
+
         for (int u = 1; u < adjList.size(); u++) {
             for (int v : adjList.get(u)) {
                 inDegree[v]++;
             }
         }
+
         for (int i = 1; i < inDegree.length; i++) {
-            if (inDegree[i] == 0) degree0.add(i);
+            if (inDegree[i] == 0) {
+                degree0.add(i);
+            }
         }
+
         while (!degree0.isEmpty()) {
-            int cur = degree0.remove();
+            int cur = degree0.poll();
             result.add(cur);
             for (int neighbor : adjList.get(cur)) {
                 inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) degree0.add(neighbor);
+                if (inDegree[neighbor] == 0) {
+                    degree0.add(neighbor);
+                }
             }
         }
+
         for (int degree : inDegree) {
             if (degree != 0) {
                 return false;
