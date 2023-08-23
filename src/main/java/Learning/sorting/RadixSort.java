df40f8ea75f8b2d1ba6bd6365e61ee041eec60b7
Tai Ha <u7454578@anu.edu.au>
2023-08-24 00:24:02 +1000

    EmployeeBonus
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

---
+++ src/main/java/Learning/sorting/RadixSort.java
@@ -0,0 +1,13 @@
+package Learning.sorting;
+
+public class RadixSort {
+    /**
+     * Used when:
+     * - alot of number but the digit is not high, fine even if the range is high
+     * -
+     *
+     * steps:
+     * - sort by digit using counting sort
+     */
+
+}
---
+++ src/main/java/leetcode/sql/EmployeeBonus.java
@@ -0,0 +1,6 @@
+package leetcode.sql;
+
+public class EmployeeBonus {
+    String res = "SELECT * FROM employee e LEFT JOIN bonus b ON e.empId = b.empId HAVING b.bonus < 1000 OR b.bonus is NULL;";
+
+}
