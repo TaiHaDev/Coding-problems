33dc57ee25204614fbe7c49bbc959c44a7cb0540
Tai Ha <u7454578@anu.edu.au>
2023-08-24 00:25:12 +1000

    EmployeeBonus
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/leetcode/sql/EmployeeBonus.java
+++ src/main/java/leetcode/sql/EmployeeBonus.java
@@ -2,5 +2,4 @@ package leetcode.sql;
 
 public class EmployeeBonus {
     String res = "SELECT * FROM employee e LEFT JOIN bonus b ON e.empId = b.empId HAVING b.bonus < 1000 OR b.bonus is NULL;";
-
 }
