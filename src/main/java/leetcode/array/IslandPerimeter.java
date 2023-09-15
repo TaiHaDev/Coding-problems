632d915f5b6c4139eb7f6b13e89132ac46f28c2a
Tai Ha <u7454578@anu.edu.au>
2023-09-15 11:47:21 +1000

    improved solution
    Signed-off-by: Tai Ha <u7454578@anu.edu.au>

--- src/main/java/leetcode/array/IslandPerimeter.java
+++ src/main/java/leetcode/array/IslandPerimeter.java
@@ -10,12 +10,8 @@ public class IslandPerimeter {
             for (int c = 0; c < cow; c++) {
                 if (grid[r][c] == 1) {
                     res += 4;
-                    if (r > 0 && grid[r-1][c] == 1) {
-                        res -= 2;
-                    }
-                    if (c > 0 && grid[r][c-1] == 1) {
-                        res -= 2;
-                    }
+                    if (r > 0 && grid[r-1][c] == 1) res-=2;
+                    if (c > 0 && grid[r][c-1] == 1) res-=2;
                 }
             }
         }
