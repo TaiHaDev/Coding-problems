516d44e558899c904d6462fb502ada6166ed3b62
Tai Ha <taihavn1101@gmail.com>
2024-03-30 01:39:02 +1100

    leetcode restart day1
    Signed-off-by: Tai Ha <taihavn1101@gmail.com>

---
+++ src/main/java/leetcode/sliding_window/LongestSubstringWithoutRepeat.java
@@ -0,0 +1,26 @@
+package leetcode.sliding_window;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestSubstringWithoutRepeat {
+    public static void main(String[] args) {
+        System.out.println(new LongestSubstringWithoutRepeat().lengthOfLongestSubstring("abcabcbb"));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        List<Character> substringL = new ArrayList<>();
+        int largestlength = 0;
+        for(int right = 0; right < s.length(); right++) {
+            if(substringL.contains(s.charAt(right))) {
+                // get the index of the char
+                int index = substringL.indexOf(s.charAt(right));
+                substringL.remove(index);
+                if(index > 0)
+                    substringL.subList(0, index).clear();
+            }
+            substringL.add(s.charAt(right));
+            largestlength = Math.max(largestlength, substringL.size());
+        }
+        return largestlength;
+    }
+}
