1950fc8d34152a3fe8fffac9fd876d59f79e95b7
Tai Ha <taihavn1101@gmail.com>
2024-04-11 19:11:12 +1000

    leetcode day3 removeElement understand
    Signed-off-by: Tai Ha <taihavn1101@gmail.com>

--- src/main/java/leetcode/array/RemoveElement27.java
+++ src/main/java/leetcode/array/RemoveElement27.java
@@ -5,19 +5,27 @@ public class RemoveElement27 {
         System.out.println(new RemoveElement27().removeElement(new int[] {0,1,2,2,3,0,4,2}, 2));
     }
     public int removeElement(int[] nums, int val) {
-        int idx = -1;
-        int count = 0;
+        int idx = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val && idx == -1) {
-                idx = i;
-            } else if (nums[i] != val && idx != -1) {
+            if (nums[i] != val) {
                 nums[idx] = nums[i];
                 idx++;
-                count++;
-            } else if (nums[i] != val) {
-                count++;
             }
         }
-        return count;
+        return idx;
     }
+    public int removeElement2(int[] nums, int val) {
+        int l = 0;
+        int h = nums.length - 1;
+        while (l <= h) {
+            if (nums[l] == val) {
+                nums[l] = nums[h];
+                h--;
+            } else {
+                l++;
+            }
+        }
+        return l;
+    }
+
 }
