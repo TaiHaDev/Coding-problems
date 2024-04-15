fd5599be174a470ca91ca23ae6119d8926306c6f
Tai Ha <taihavn1101@gmail.com>
2024-04-15 21:17:21 +1000

    leetcode day5: map
    Signed-off-by: Tai Ha <taihavn1101@gmail.com>

--- src/main/java/leetcode/array/GroupAnagram49.java
+++ src/main/java/leetcode/array/GroupAnagram49.java
@@ -1,7 +1,6 @@
 package leetcode.array;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class GroupAnagram49 {
     public static void main(String[] args) {
@@ -9,15 +8,12 @@ public class GroupAnagram49 {
         System.out.println(groupAnagrams(s));
     }
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap();
         for (int i = 0; i < strs.length; i++) {
-            int[] charCount = new int[26];
-            for (char c : strs[i].toCharArray()) {
-                charCount[c - 'a'] += 1;
-            }
-            List<String> li = map.getOrDefault(Arrays.toString(charCount), new ArrayList<>());
-            li.add(strs[i]);
-            map.put(Arrays.toString(charCount), li);
+            char[] sorted = strs[i].toCharArray();
+            Arrays.sort(sorted);
+            map.putIfAbsent(Arrays.toString(sorted), new ArrayList());
+            map.get(Arrays.toString(sorted)).add(strs[i]);
         }
         return map.values().stream().toList();
     }
