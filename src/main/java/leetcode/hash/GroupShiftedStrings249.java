4715b16c46756a5c15e6f6ca133ac7803caee564
Tai Ha <taihavn1101@gmail.com>
2024-04-16 00:04:08 +1000

    leetcode day5: map
    Signed-off-by: Tai Ha <taihavn1101@gmail.com>

---
+++ src/main/java/leetcode/hash/GroupShiftedStrings249.java
@@ -0,0 +1,21 @@
+package leetcode.hash;
+
+import java.util.*;
+
+public class GroupShiftedStrings249 {
+    public static void main(String[] args) {
+        System.out.println(new GroupShiftedStrings249().groupStrings(new String[] {"abc","bcd","acef","xyz","az","ba","a","z"}));
+    }
+    public List<List<String>> groupStrings(String[] strings) {
+        Map<String, List<String>> map = new HashMap();
+        for (String str : strings) {
+            char[] curStr = str.toCharArray();
+            int offset = curStr[0] - 'a';
+            for (int i = 0; i < curStr.length; i++) {
+                curStr[i] = (char) ((curStr[i] - offset + 26) % 26 + 'a');
+            }
+            map.computeIfAbsent(Arrays.toString(curStr), k -> new ArrayList()).add(str);
+        }
+        return map.values().stream().toList();
+    }
+}
