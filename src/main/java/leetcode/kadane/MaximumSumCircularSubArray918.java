864150658898f93b40119c20a3735bbbac7108ff
Tai Ha <taihavn1101@gmail.com>
2024-04-15 19:43:14 +1000

    leetcode day5: Kadane
    Signed-off-by: Tai Ha <taihavn1101@gmail.com>

--- src/main/java/leetcode/kadane/MaximumSumCircularSubArray918.java
+++ src/main/java/leetcode/kadane/MaximumSumCircularSubArray918.java
@@ -1,4 +1,27 @@
 package leetcode.kadane;
 
 public class MaximumSumCircularSubArray918 {
+    public static void main(String[] args) {
+        System.out.println(new MaximumSumCircularSubArray918().maxSubarraySumCircular(new int[] {5,-3,5}));
+    }
+    public int maxSubarraySumCircular(int[] nums) {
+        int totalSum = 0;
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int curMax = 0;
+        int curMin = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+            curMax = Math.max(nums[i], curMax + nums[i]);
+            curMin = Math.min(nums[i], curMin + nums[i]);
+            maxSum = Math.max(maxSum, curMax);
+            minSum = Math.min(curMin, minSum);
+        }
+        if (minSum == totalSum) {
+            return maxSum;
+        } else {
+            return Math.max(maxSum, totalSum - minSum);
+        }
+
+    }
 }
